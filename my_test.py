ad86bf05ad0416fe4c0b303dfe5b11cac30d3b6f
Mike Rossetti <s2t2@users.noreply.github.com>
2019-03-06 16:19:49 -0500

    Passing tests in top-level file

--- my_test.py
+++ my_test.py
@@ -1,3 +1,8 @@
 
-def test_something():
-    assert True == True
+from my_script import message
+
+# def test_something():
+#     assert True == True
+
+def test_message():
+    assert message() == "HELLO FROM A SCRIPT"
