6e654827489ad7b755cbe4a984d1a82950f6863f
Mike Rossetti <s2t2@users.noreply.github.com>
2019-03-06 16:23:34 -0500

    WIP - Err: no module named app

--- test/nested_test.py
+++ test/nested_test.py
@@ -1,4 +1,8 @@
 
+from app.nested_script import nested_message
 
-def test_something_else():
-    assert True == False
+# def test_something_else():
+#     assert True == True
+
+def test_nested_message():
+    assert nested_message() == "HELLO FROM A SCRIPT"
