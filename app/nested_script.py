3a22c6bf32483373b33db5fae08cdeff3cbb9cd9
Mike Rossetti <s2t2@users.noreply.github.com>
2019-03-06 15:45:47 -0500

    Add example functions

---
+++ app/nested_script.py
@@ -0,0 +1,6 @@
+
+def nested_message():
+    return "HELLO FROM A SCRIPT IN THE APP DIR"
+
+if __name__ == "__main__":
+    print(nested_message())
---
+++ my_script.py
@@ -0,0 +1,6 @@
+
+def message():
+    return "HELLO FROM A SCRIPT"
+
+if __name__ == "__main__":
+    print(message())
